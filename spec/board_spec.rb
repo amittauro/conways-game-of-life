72bca882e5e0e781018bd156bd11b795c582482f
Amit Tauro <tauroamit@gmail.com>
2021-01-11 10:45:02 +0000

    updating tests

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,11 +1,11 @@
 require './lib/board'
 
 describe Board do
-  describe '#run' do
-    let(:cell1) { double('cell', :live => true) }
-    let(:cell2) { double('cell', :live => false) }
+  let(:cell1) { double('cell', :live => true) }
+  let(:cell2) { double('cell', :live => false) }
+  describe '#survives' do
     context 'when the cente cell is alive and survives' do
-      it 'returns true for 2 neighbours' do
+      it 'returns true for 2 live neighbours' do
         board = Board.new([
           [cell1, cell2, cell1],
           [cell2, cell1, cell2],
@@ -14,7 +14,7 @@ describe Board do
         expect(board.survives?).to eq(true)
       end
 
-      it 'returns true with 3 neighbours' do
+      it 'returns true with 3 live neighbours' do
         board = Board.new([
           [cell1, cell2, cell1],
           [cell2, cell1, cell2],
