3eea2e7d09f2a9bcebaba101660b54716fa90bb8
Amit Tauro <tauroamit@gmail.com>
2021-01-11 10:40:09 +0000

    updating first test

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -2,10 +2,10 @@ require './lib/board'
 
 describe Board do
   describe '#run' do
+    let(:cell1) { double('cell', :live => true) }
+    let(:cell2) { double('cell', :live => false) }
     context 'when there are 2 or 3 neighbours' do
-      it 'returns a live cell' do
-        cell1 = double('cell', :live => true)
-        cell2 = double('cell', :live => false)
+      it 'returns a live cell for 2 neighbours' do
         board = Board.new([
           [cell1, cell2, cell1],
           [cell2, cell1, cell2],
