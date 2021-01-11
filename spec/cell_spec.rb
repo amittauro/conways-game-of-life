b4cc44a1c981ad819348583a48caacef38340f46
Amit Tauro <tauroamit@gmail.com>
2021-01-11 15:10:03 +0000

    test for dead cell to be dead

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -6,5 +6,10 @@ describe Cell do
       cell = Cell.new(true)
       expect(cell.live).to eq(true)
     end
+
+    it 'returns false starting dead' do
+      cell = Cell.new(false)
+      expect(cell.live).to eq(false)
+    end
   end
 end
